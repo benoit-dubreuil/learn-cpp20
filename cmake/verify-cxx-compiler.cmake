ca0f1ed1e0f0bd750c6f13235e3fc4adadbfc5e7
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-06-07 14:41:21 -0400

    Fixed include

--- cmake/verify-cxx-compiler.cmake
+++ cmake/verify-cxx-compiler.cmake
@@ -1,6 +1,7 @@
 include_guard(GLOBAL)
 
-include("assure-cxx-compiler-is-msvc-cl") # TODO
+include("./assure-cxx-compiler-is-msvc-cl.cmake")
 
 function(verify_cxx_compiler)
+    # TODO
 endfunction()
