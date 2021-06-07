b4ff817f58faae0bcf508f532ac4c9275209da91
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-06-07 15:07:16 -0400

    Renamed variable

--- cmake/verify-cxx-compiler/compiler/msvc/check_is_msvc_known.cmake
+++ cmake/verify-cxx-compiler/compiler/msvc/check_is_msvc_known.cmake
@@ -3,11 +3,11 @@ include_guard(GLOBAL)
 function(check_is_msvc_known is_msvc_known_param)
     find_program(MSVC_CL "cl")
 
-    set(IS_MSVC_KNOWN_VAL DEFINED MSVC_CL)
+    set(IS_MSVC_KNOWN DEFINED MSVC_CL)
 
-    if (NOT IS_MSVC_KNOWN_VAL)
+    if (NOT IS_MSVC_KNOWN)
         message(AUTHOR_WARNING "MSVC CL not found.")
     endif ()
 
-    set("${is_msvc_known_param}" IS_MSVC_KNOWN_VAL PARENT_SCOPE)
+    set("${is_msvc_known_param}" IS_MSVC_KNOWN PARENT_SCOPE)
 endfunction()
