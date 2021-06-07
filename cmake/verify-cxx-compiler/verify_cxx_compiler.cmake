1cf98d7658c115f38f1d1bb9fd57ff721f2a09b8
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-06-07 17:01:12 -0400

    First draft of `verify_cxx_compiler` func

--- cmake/verify-cxx-compiler/verify_cxx_compiler.cmake
+++ cmake/verify-cxx-compiler/verify_cxx_compiler.cmake
@@ -3,6 +3,34 @@ include_guard(GLOBAL)
 include("compiler/msvc/assure_cxx_env_is_msvc.cmake")
 
 function(verify_cxx_compiler)
-    # TODO
+
+    if (DEFINED CACHE{CMAKE_CXX_COMPILER})
+        string(TOLOWER CACHE{CMAKE_CXX_COMPILER} lowered_cmake_cxx)
+    endif ()
+
+    if (lowered_env_cxx)
+        if (lowered_env_cxx STREQUAL "cl")
+            message(STATUS "Assure CXX ENV is MSVC")
+            assure_cxx_env_is_msvc()
+        endif ()
+        
+        return()
+    endif ()
+
+    if (DEFINED ENV{CXX})
+        string(TOLOWER ENV{CXX} lowered_env_cxx)
+    endif ()
+
+    if (lowered_cmake_cxx)
+        if (lowered_env_cxx STREQUAL "cl")
+            message(STATUS "Assure CXX ENV is MSVC")
+            assure_cxx_env_is_msvc()
+        endif ()
+
+        return()
+    endif ()
+
+    message(AUTHOR_WARNING "No defined compiler to verify.")
+
 endfunction()
 
