9718914b748533e640daee746a2e137954ab9ed6
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-06-07 12:18:55 -0400

    Encapsulated the script into a function

--- cmake/find-msvc-cl.cmake
+++ cmake/find-msvc-cl.cmake
@@ -2,35 +2,40 @@
 
 include_guard(GLOBAL)
 
-file(GLOB vs_version_dirs
-     LIST_DIRECTORIES true
-     "C:/Program Files (x86)/Microsoft Visual Studio/[0-9]*")
+function(find_msvc_cl msvc_cl_ptr)
+    file(GLOB vs_version_dirs
+         LIST_DIRECTORIES true
+         "C:/Program Files (x86)/Microsoft Visual Studio/[0-9]*")
 
-if (NOT vs_version_dirs)
-    message(FATAL_ERROR "Visual Studio is not installed.")
-endif ()
+    if (NOT vs_version_dirs)
+        message(AUTHOR_WARNING "Visual Studio is not installed.")
+        return()
+    endif ()
 
-list(GET vs_version_dirs -1 newest_vs_version_dir)
+    list(GET vs_version_dirs -1 newest_vs_version_dir)
 
-file(GLOB msvc_version_dirs
-     LIST_DIRECTORIES true
-     "${newest_vs_version_dir}/Community/VC/Tools/MSVC/*")
+    file(GLOB msvc_version_dirs
+         LIST_DIRECTORIES true
+         "${newest_vs_version_dir}/Community/VC/Tools/MSVC/*")
 
-if (NOT msvc_version_dirs)
-    message(FATAL_ERROR "MSVC is not installed.")
-endif ()
+    if (NOT msvc_version_dirs)
+        message(AUTHOR_WARNING "MSVC is not installed.")
+        return()
+    endif ()
 
-list(GET msvc_version_dirs -1 newest_msvc_version_dir)
+    list(GET msvc_version_dirs -1 newest_msvc_version_dir)
 
-set(newest_msvc_version_arch_dir "${newest_msvc_version_dir}/bin/Hostx64/x64")
+    set(newest_msvc_version_arch_dir "${newest_msvc_version_dir}/bin/Hostx64/x64")
 
-find_program(MSVC_CL
-             "cl"
-             PATHS "${newest_msvc_version_arch_dir}"
-             NO_DEFAULT_PATH)
+    find_program(MSVC_CL
+                 "cl"
+                 PATHS "${newest_msvc_version_arch_dir}"
+                 NO_DEFAULT_PATH)
 
-if (NOT MSVC_CL)
-    message(FATAL_ERROR "MSVC CL not found.")
-endif ()
+    if (NOT MSVC_CL)
+        message(AUTHOR_WARNING "MSVC CL not found.")
+        return()
+    endif ()
 
-message("${MSVC_CL}")
+    set("${msvc_cl_ptr}" "${MSVC_CL}" PARENT_SCOPE)
+endfunction()
