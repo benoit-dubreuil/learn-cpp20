73140f147ace4c10dd63a93d11891bee9a81b3bc
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-06-07 12:26:40 -0400

    Added `is-msvc-cl-known.cmake` script

---
+++ cmake/is-msvc-cl-known.cmake
@@ -0,0 +1,13 @@
+include_guard(GLOBAL)
+
+function(is_msvc_cl_known is_msvc_cl_known_param)
+    find_program(MSVC_CL
+                 "cl")
+
+    if (NOT MSVC_CL)
+        message(AUTHOR_WARNING "MSVC CL not found.")
+        return()
+    endif ()
+
+    set("${is_msvc_cl_known_param}" "${MSVC_CL}" PARENT_SCOPE)
+endfunction()
