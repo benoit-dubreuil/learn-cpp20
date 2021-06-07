6431a363467c739a9a7810217dc6b99bca3799ab
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2021-06-07 15:33:37 -0400

    Set CXX to MSVC CL if necessary

--- cmake/verify-cxx-compiler/compiler/msvc/assure_cxx_compiler_is_msvc.cmake
+++ cmake/verify-cxx-compiler/compiler/msvc/assure_cxx_compiler_is_msvc.cmake
@@ -5,5 +5,14 @@ include("find_msvc.cmake")
 include("assure_cxx_compiler_is_msvc.cmake")
 
 function(assure_cxx_compiler_is_msvc)
-    # TODO
+    check_is_msvc_known(is_msvc_known)
+
+    if (NOT is_msvc_known)
+        find_msvc(msvc_cl)
+
+        if (msvc_cl)
+            set(ENV{CXX} "${msvc_cl}")
+        endif ()
+    endif ()
+
 endfunction()
